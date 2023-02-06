e7f1b52c8d4dfbbb62b4522493825e69b8ae4e2e
Kord Campbell <kordless@gmail.com>
2023-02-05 21:05:17 -0600

    config name

--- weaviate/weaviate_test.py
+++ weaviate/weaviate_test.py
@@ -2,7 +2,7 @@ import config
 import weaviate
 
 client = weaviate.Client(
-    url="http://weaviate:f00bar@34.82.10.184:8088",
+    url="http://weaviate:f00bar@34.168.7.148:8088",
     additional_headers={
         "X-OpenAI-Api-Key": config.openai_token
     }
